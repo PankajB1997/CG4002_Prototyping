311a96ee29d39b46759cac68bb729583c9aae680
PankajB1997 <pankajbhootra@gmail.com>
2019-04-30 18:35:43 +0800

    Add new data collection script for 6 blunos (3 dancers) and update documentation

--- Raspberry_Pi/data_collection.py
+++ Raspberry_Pi/data_collection.py
@@ -1,73 +1,157 @@
-#!/usr/bin/python3
-import time
-import serial
 import os
-import sys
-import threading
+from bluepy.btle import Scanner, DefaultDelegate, Peripheral
+import datetime
+from collections import deque
+import datetime
+import serial
 import socket
+import sys
+import time
+from Crypto import Random
+from Crypto.Cipher import AES
 import base64
-import pickle
+import threading
 
-N = 128
-count = 1
+SAVEPATH = os.path.join("..", "dataset", "RawData")
+DANCE_MOVE = "chicken"
+DANCER_1 = "ashley"
+DANCER_2 = "hazmei"
+DANCER_3 = "pankaj"
 
-danceMove = "number7"
-dancer = "jin"
-SAVEPATH = os.path.join("dataset", "RawData", dancer, danceMove + ".txt")
+useServer = 0
+collect_test_data = 0
+testing_samples = 1
 
-def readLineCR(port):
-    rv = ""
-    while True:
-        ch = port.read().decode()
-        rv += ch
-        if ch == "\r" or ch == "":
-            return rv
+# Add all 6 bluno addresses here as a list, in the given order
+bt_addrs = ["0c:b2:b7:46:57:50", # dancer 1 left hand bluno
+            "0c:b2:b7:46:35:f5", # dancer 1 right hand bluno
+            "0c:b2:b7:46:4d:80", # dancer 2 left hand bluno
+            "0c:b2:b7:46:35:96", # dancer 2 right hand bluno
+            "0c:b2:b7:46:39:a6", # dancer 3 left hand bluno
+            "0c:b2:b7:46:41:67"] # dancer 3 right hand bluno
+connections = []
+connection_threads = []
+message_buffer = []
 
-def testReadLineCR(port):
-    read_flag = 1
-    rv = ""
-    while (read_flag == 1):
-        ch = port.read()
-        rv += ch
-        if ch == "\r":
-            data = rv
-            read_flag = 0
+class Data():
+    def __init__(self, socket):
+        self.bs = 32
+        self.secret_key = "1234512345123451"
+        self.voltage = 0
+        self.current = 0
+        self.power = 0  # voltage * current
+        self.cumpower = 0
+        self.sock = socket
+        self.sample_queue = deque([], 20)
 
-handshake_flag = False
-data_flag = False
-print("test")
-port = serial.Serial("/dev/serial0", baudrate=115200, timeout=3.0)
-print("set up")
-port.reset_input_buffer()
-port.reset_output_buffer()
-# port.flushInput()
-# port.flushOutput()
+    def pad(self, msg):
+        return msg + (self.bs - len(msg)%self.bs)*chr(self.bs - len(msg)%self.bs)
 
-while (handshake_flag == False):
-    port.write("H".encode())
-    print("H sent")
-    response = port.read(1)
-    time.sleep(0.5)
-    if (response.decode() == "A"):
-        print("A received, sending N")
-        port.write("N".encode())
-        time.sleep(0.5)
-        handshake_flag= True
-        # port.readline()
-    else:
-        time.sleep(0.5)
+    def encryptText(self, msg, secret_key):
+        raw = self.pad(msg)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(secret_key,AES.MODE_CBC,iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
 
-print("connected")
-# port.flush() #waits till all in buffer is written then flush
+    def sendData(self, predictedMove):
+        formattedAnswer = ("#"+ str(predictedMove) + "|" + str(self.voltage)\
+            + "|" + str(self.current) + "|" + str(self.power) + "|" + str(self.cumpower) + "|")
+        print(formattedAnswer)
+        encryptedText = self.encryptText(formattedAnswer, self.secret_key)
+        self.sock.send(encryptedText)
 
-while (data_flag == False):
-    print("ENTERING")
-    with open(SAVEPATH, "a") as txtfile:
-        for i in range(N): # print from 0->127 = 128 sets of readings
-            data = readLineCR(port)
-            data = data.split(",")[0:9] # extract acc1[3], acc2[3] and gyro[3] values
-            data = [ val.strip() for val in data ]
-            output = "\t".join(data) + "\n"
-            txtfile.write(output)
-            print(str(count) + ". " + output)
-            count += 1
+class NotificationDelegate(DefaultDelegate):
+    def __init__(self, number):
+        DefaultDelegate.__init__(self)
+        self.number = number
+        self.message_string = ""
+
+    def handleNotification(self, cHandle, data):
+        msg = data.decode("utf-8")
+        # print("Notification:\nConnection:" + str(self.number) + "\nMsg:" + msg)
+        self.message_string = self.message_string + msg
+        if "\n" in msg: # if end of message
+            message_buffer[self.number].append(self.message_string)
+            file_name = "data{}".format(self.number)
+            my_file = open(file_name, "a+")
+            my_file.write(self.message_string)
+            self.message_string = ""
+
+class ConnectionHandlerThread(threading.Thread):
+    def __init__(self, connection_index):
+        threading.Thread.__init__(self)
+        self.connection_index = connection_index
+
+    def run(self):
+        connection = connections[self.connection_index]
+        connection.setDelegate(NotificationDelegate(self.connection_index))
+        port = connection.getServiceByUUID("0000dfb0-0000-1000-8000-00805f9b34fb")
+        dfb1 = port.getCharacteristics()[0]
+        connection.writeCharacteristic((dfb1.getHandle() + 1), b"\x01\x00")
+
+        dfb1.write(bytes("H", "utf-8"))
+        print("H sent")
+
+        while True:
+            if connection.waitForNotifications(1):
+                continue
+
+class RaspberryPi():
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.isHandshakeDone = False
+        self.result_queue = deque([], 2)
+
+        self.prev_data_time = time.time()
+        self.current_data_time = time.time()
+
+    def connectToServer(self):
+        IPAddress = sys.argv[1]
+        Port = int(sys.argv[2])
+        server_address = (IPAddress, Port)
+        self.sock.connect(server_address)
+
+    def connectToBlunos(self):
+        print("Connecting to blunos ...")
+        for addr in bt_addrs:
+            p = Peripheral(addr)
+            connections.append(p)
+            message_buffer.append([])
+            t = ConnectionHandlerThread(len(connections)-1)
+            t.start()
+            connection_threads.append(t)
+        print("Ports Open!")
+
+    def collectDancerData(bluno_left_idx, bluno_right_idx):
+        first_string = message_buffer[bluno_left_idx][0].strip("\n")
+        second_string = message_buffer[bluno_right_idx][0].strip("\n")[:-1] #Remove the last \n and comma
+        full_msg = first_string + second_string + "\n"
+        dancer_savepath = os.path.join(SAVEPATH, dancer, DANCE_MOVE)
+        with open(dancer_savepath, "a+") as f:
+            f.write(full_msg)
+        message_buffer[bluno_left_idx] = message_buffer[bluno_left_idx][1:]
+        message_buffer[bluno_right_idx] = message_buffer[bluno_right_idx][1:]
+
+    def run(self):
+        try:
+            # Set up connections to test server and blunos
+            if(useServer):
+                self.connectToServer()
+                print("Connected to test server")
+                data = Data(self.sock)
+            self.connectToBlunos()
+            # Main loop to run data collection
+            while 1:
+                if len(message_buffer[0]) > 0 and len(message_buffer[1]) > 0:
+                    self.collectDancerData(0, 1)
+                if len(message_buffer[2]) > 0 and len(message_buffer[3]) > 0:
+                    self.collectDancerData(2, 3)
+                if len(message_buffer[4]) > 0 and len(message_buffer[5]) > 0:
+                    self.collectDancerData(4, 5)
+                continue
+        except KeyboardInterrupt:
+            sys.exit(1)
+
+if __name__ == "__main__":
+    pi = RaspberryPi()
+    pi.run()
--- docs/Software.md
+++ docs/Software.md
@@ -6,16 +6,15 @@ The software part consists of two main components: the machine learning aspect t
 
 The Machine Learning scripts are added to the `Raspberry_Pi/` folder. A detailed list of all the ML scripts is as below:  
 
-1. `data_collection.py` - This script interfaces with one Arduino Mega board to receive sensor readings every 20 ms. This script is used for collecting data to train/test the model. Data for each dancer and each dance move is saved to the path `dataset\RawData\<dancer>\<danceMove>.txt` where `<dancer>` and `<danceMove>` are initialised in the beginning of this script and can thus be configured.
+1. `data_collection.py` - This script interfaces with 6 Bluno boards to receive sensor readings for 3 dancers every 20 ms. This script is used for collecting data to train/test the model. Data for each dancer and each dance move is saved to the path `dataset\RawData\<dancer>\<danceMove>.txt` where `<dancer>` and `<danceMove>` are initialised in the beginning of this script and can thus be configured.
 2. `raw_data_categorize_by_move.py` - This script takes in the data collected using `data_collection.py` and consolidates it across different dancers for each dance move and generates a pickle object as output for feature extraction.
 3. `feature_extraction.py` - This script takes in the pickle file generated from `raw_data_categorize_by_move.py` and prepares a training and testing set from the same by applying preprocessing and feature engineering steps on the raw dataset. For preprocessing, lowpass filter, highpass filter, and normalization steps are applied. For feature engineering, both time domain and frequency domain features are extracted. The training and testing sets are saved as pickle files.
 4. `train_classifier.py` - This script takes in the training and testing dataset from `feature_extraction.py` and trains a selected list of sklearn ML models on the same and reports the performance of each, in terms of accuracy, precision, recall and f1 score. The trained models are saved as pickle files.
 5. `run_detector.py` - This is the script for real-time prediction of dance moves using a pretrained model. This script interfaces  with one Arduino Mega board to receive sensor readings every 20 ms and tries to predict what dance move is being performed with steps such as preprocessing, feature engineering and model prediction.
 
 The following files are work in progress and need to be improved/continued in future:  
-1. `data_collection_6_blunos.py` - To interface with 6 blunos concurrently to collect sensor data from each and store it .
-2. `main.py` - Communications code to interface with blunos using Bluetooth to connect to each and receive sensor readings every 20 ms.
-3. `final_eval_server.py` - Server code to be used by the evaluator. Currently, this version is for evaluating systems as per the specs of CG3002. However, this needs to be updated as per the specs of CG4002.
+1. `main.py` - Communications code to interface with blunos using Bluetooth to connect to each and receive sensor readings every 20 ms.
+2. `final_eval_server.py` - Server code to be used by the evaluator. Currently, this version is for evaluating systems as per the specs of CG3002. However, this needs to be updated as per the specs of CG4002.
 
 ## Coaching Dashboard
 
